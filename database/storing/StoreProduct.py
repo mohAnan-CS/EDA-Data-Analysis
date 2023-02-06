661f7590f1aca333990c8739dd1b82285215b28d
Mohammad Anan <smoker_shuger@hotmail.com>
2023-02-06 15:46:36 +0200

    store product information to database

---
+++ database/storing/StoreProduct.py
@@ -0,0 +1,24 @@
+def store_all(list_product, cursor, mydb):
+    for data in list_product:
+        product_id = data['product_id']
+        product_found = product_is_found(str(product_id), cursor)
+        if not product_found:
+            store_product(data['product_id'], data['product_type'], data['product'], data['current_retail_price'], cursor, mydb)
+
+
+def store_product(product_id, product_type, product, current_retail_price, cursor, mydb):
+    insert_product_sql = "insert into products (product_id, product_type, product, current_retail_price) VALUES (%s, %s, %s, %s)"
+    price = float(current_retail_price.replace("$", ""))
+    values = (str(product_id), product_type, product, price)
+    cursor.execute(insert_product_sql, values)
+    mydb.commit()
+
+
+def product_is_found(product_id, cursor):
+    check_product_sql = "select * from products where product_id = " + product_id + ";"
+    cursor.execute(check_product_sql)
+    product_records = cursor.fetchall()
+    if len(product_records) >= 1:
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from database import Connector
 from file_operation import ReadCustomer, ReadProduct, ReadSalesOutlet, ReadSalesTarget
-from database.storing import StoreCustomer
+from database.storing import StoreCustomer, StoreProduct
 
 connection = Connector.connect("root", "localhost", "0000")
 database_cursor = connection.cursor()
@@ -16,10 +16,12 @@ database_cursor.execute("USE SalesDataBase")
 
 # ----------------- Read data files ----------------------
 
-customer_list = ReadCustomer.read_customer_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\customer.csv")
-ReadProduct.read_product_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\product.csv")
-ReadSalesOutlet.read_sales_outlet_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales_outlet.csv")
-ReadSalesTarget.read_sales_target_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales targets.csv")
+# customer_list = ReadCustomer.read_customer_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\customer.csv")
+product_list = ReadProduct.read_product_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\product.csv")
+print(product_list)
+# ReadSalesOutlet.read_sales_outlet_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales_outlet.csv")
+# ReadSalesTarget.read_sales_target_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales targets.csv")
 
 # ------------------ Store data files ---------------------
-StoreCustomer.store_all(customer_list, database_cursor, connection)
+# StoreCustomer.store_all(customer_list, database_cursor, connection)
+StoreProduct.store_all(product_list, database_cursor, connection)
