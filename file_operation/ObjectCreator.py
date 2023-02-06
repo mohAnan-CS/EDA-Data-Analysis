80c87aef322b32d392427d0de5db7d608aeb4ce8
Mohammad Anan <smoker_shuger@hotmail.com>
2023-02-06 12:22:46 +0200

    add create product object function in ObjectCreator class

--- file_operation/ObjectCreator.py
+++ file_operation/ObjectCreator.py
@@ -7,3 +7,13 @@ def create_customer_obj(customer_id, customer_first_name, customer_since, birthd
         "gender": gender,
     }
     return customer_item
+
+
+def create_product_obj(product_id, product_type, product, current_retail_price):
+    product_obj = {
+        "product_id": product_id,
+        "product_type": product_type,
+        "product": product,
+        "current_retail_price": current_retail_price,
+    }
+    return product_obj
