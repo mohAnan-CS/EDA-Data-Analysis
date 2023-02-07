34ae32a5e6153d7feb554eee23a5069a32e147aa
Mohammad Anan <smoker_shuger@hotmail.com>
2023-02-07 07:24:27 +0200

    store all transaction info (transaction, transaction_details, transaction_product)

--- database/storing/StoreTransaction.py
+++ database/storing/StoreTransaction.py
@@ -3,16 +3,55 @@ from database.storing import StoreCustomer, StoresSalesOutlet
 
 def store_all(list_transaction, cursor, mydb):
     for data in list_transaction:
+        transaction_id = data['transaction_id']
         customer_id = data['customer_id']
         sales_outlet_id = data['sales_outlet_id']
+
         customer_found = StoreCustomer.customer_is_found(str(customer_id), cursor)
         sales_outlet_found = StoresSalesOutlet.sales_outlet_is_found(str(sales_outlet_id), cursor)
+        transaction_found = transaction_is_found(transaction_id, cursor)
+
         if customer_found and sales_outlet_found:
-            store_transaction(data['transaction_id'], data['instore_yn'], data['customer_id'], data['sales_outlet_id'], cursor, mydb)
+            if not transaction_found:
+                store_transaction(data['transaction_id'], data['instore_yn'], data['customer_id'], data['sales_outlet_id'], cursor, mydb)
+                store_transaction_details(data['transaction_id'], data['transaction_date'], data['transaction_time'], cursor, mydb)
+            else:
+                store_transaction_product(data['transaction_id'], data['quantity'], data['line_item_amount'], data['product_id'], cursor, mydb)
 
 
 def store_transaction(transaction_id, instore_yn, customer_id, sales_outlet_id, cursor, mydb):
+    in_store = True
+    if instore_yn == 'Y':
+        in_store = True
+    elif instore_yn == 'N':
+        in_store = False
+
     insert_transaction_sql = "insert into transaction (transaction_id, instore_yn, customer_id, sales_outlet_id) VALUES (%s, %s, %s, %s)"
-    values = (str(transaction_id), str(instore_yn), str(customer_id), str(sales_outlet_id))
+    values = (str(transaction_id), in_store, str(customer_id), str(sales_outlet_id))
+
     cursor.execute(insert_transaction_sql, values)
     mydb.commit()
+
+
+def store_transaction_product(transaction_id, quantity, line_item_amount, product_id, cursor, mydb):
+    insert_transaction_product_sql = "insert into transaction_product (transaction_id, quantity, line_item_amount, product_id) VALUES (%s, %s, %s, %s)"
+    values = (str(transaction_id), str(quantity), str(line_item_amount), str(product_id))
+    cursor.execute(insert_transaction_product_sql, values)
+    mydb.commit()
+
+
+def store_transaction_details(transaction_id, transaction_date, transaction_time, cursor, mydb):
+    insert_transaction_details_sql = "insert into transaction_details (transaction_id, transaction_date, transaction_time) VALUES (%s, %s, %s)"
+    values = (str(transaction_id), transaction_date, transaction_time)
+    cursor.execute(insert_transaction_details_sql, values)
+    mydb.commit()
+
+
+def transaction_is_found(transaction_id, cursor):
+    check_transaction_sql = "select * from transaction where transaction_id = " + str(transaction_id) + ";"
+    cursor.execute(check_transaction_sql)
+    transaction_records = cursor.fetchall()
+    if len(transaction_records) >= 1:
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from database import Connector
 from file_reader import ReadCustomer, ReadProduct, ReadSalesOutlet, ReadSalesTarget, ReadTransaction
-from database.storing import StoreCustomer, StoreProduct, StoresSalesOutlet, StoreSalesTarget
+from database.storing import StoreCustomer, StoreProduct, StoresSalesOutlet, StoreSalesTarget, StoreTransaction
 
 connection = Connector.connect("root", "localhost", "0000")
 database_cursor = connection.cursor()
@@ -11,17 +11,17 @@ database_cursor.execute("USE SalesDataBase")
 
 # ----------------- Read data files ----------------------
 
-# customer_list = ReadCustomer.read_customer_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\customer.csv")
-# product_list = ReadProduct.read_product_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\product.csv")
-# sales_outlet_list = ReadSalesOutlet.read_sales_outlet_file(
-#   "C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales_outlet.csv")
-# sales_target_list = ReadSalesTarget.read_sales_target_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales targets.csv")
+customer_list = ReadCustomer.read_customer_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\customer.csv")
+product_list = ReadProduct.read_product_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\product.csv")
+sales_outlet_list = ReadSalesOutlet.read_sales_outlet_file(
+    "C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales_outlet.csv")
+sales_target_list = ReadSalesTarget.read_sales_target_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\sales targets.csv")
 transaction_list = ReadTransaction.read_transaction_file("C:\\Users\\twitter\\PycharmProjects\\ProjectTrainingAsal\\files_data\\201904 sales reciepts.csv")
-print(transaction_list[0])
 # ---------------------------------------------------------
 
 # ------------------ Store data files to database---------------------
-# StoreCustomer.store_all(customer_list, database_cursor, connection)
-# StoreProduct.store_all(product_list, database_cursor, connection)
-# StoresSalesOutlet.store_all(sales_outlet_list, database_cursor, connection)
-# StoreSalesTarget.store_all(sales_target_list, database_cursor, connection)
+StoreCustomer.store_all(customer_list, database_cursor, connection)
+StoreProduct.store_all(product_list, database_cursor, connection)
+StoresSalesOutlet.store_all(sales_outlet_list, database_cursor, connection)
+StoreSalesTarget.store_all(sales_target_list, database_cursor, connection)
+StoreTransaction.store_all(transaction_list, database_cursor, connection)
